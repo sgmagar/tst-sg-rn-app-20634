8cad0d3bcba1821620ef1930ffed8e3eb15d8112
Saurav <sp.gharti@gmail.com>
2020-09-24 16:09:27 +0000

    Initial commit.

---
+++ ios/tst_sg_rn_app_20634Tests/tst_sg_rn_app_20634Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface tst_sg_rn_app_20634 : XCTestCase
+
+@end
+
+@implementation tst_sg_rn_app_20634
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
